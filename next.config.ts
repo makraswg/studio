c818b95888cc388e58a2ae81da65f3022b826d45
SWG mbH Nordhausen <swg.nordhausen@gmail.com>
2026-02-09 15:26:27 +0000

    44.41    Generating static pages (0/52) ... 45.26 Error occurred prerendering page "/settings/email". Read more: https://nextjs.org/docs/messages/prerender-error 45.26 ReferenceError: Switch is not defined 45.26     at v (.next/server/app/(dashboard)/settings/email/page.js:1:3224) { 45.26   digest: '1940787928' 45.26 } 45.26 Export encountered an error on

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
